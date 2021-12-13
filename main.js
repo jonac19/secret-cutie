5b886a95bcee4df553a3c56512234372dee926fa
jonac19 <jonac19@uci.edu>
2021-12-12 19:19:45 -0800

    sendMail function uses refresh token to send emails

--- main.js
+++ main.js
@@ -4,17 +4,14 @@ const { google } = require('googleapis');
 const CLIENT_ID = '1044622659958-9m1bmqsi9956mkoa5aek32rt714elijv.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04zfZWwcdtdQNCgYIARAAGAQSNwF-L9IrNDqXn845Z6wZJCrvnzB8Pyuw6QhklaSX8DoWty4sEjNhe5cGCPn6ILsIee8U4vWf4ks';
+const REFRESH_TOKEN = '1//04oyg6gLBcfZFCgYIARAAGAQSNwF-L9IrWIxeCSQwblklaCQCC-dRV21P2RVO_0a-342abprVt8Br74l7QTaCUQA2oAqk77P4C7U';
 
-const OAuth2 = google.auth.OAuth2
-const oAuth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+const OAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
 
-async function sendMail(){
+function sendMail(){
     try{
-        //const accessToken = oAuth2Client.getAccessToken()
-        const accessToken = 'ya29.a0ARrdaM_frJGmRjcGlSa42B4k5b-CY16HbMkoPRRvFX9eseOtKCSRQL-fNHEyiuXoedZaGUMXPQkXe6liBjD-p4SJzdkyMr-ieVTdE3h5kJYiKoKbM7BtOJdeIJYQixe66uDD-lIcehjYg0bc4MeWbKzkbNxS'
-
+        const accessToken = OAuth2Client.getAccessToken()
         const transport = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -28,11 +25,10 @@ async function sendMail(){
         })
 
         const mailOptions = {
-            from: 'SECRET CUTIE TEST <secretcutietest@gmail.com>',
+            from: 'Secret Cutie <secretcutietest@gmail.com>',
             to: 'secretcutietest@gmail.com',
-            subject: 'SECRET CUTIE TEST TEST',
-            text: 'PLEASE WORK',
-            html: '<h1>PLEASE WORK</h1>'
+            subject: 'Secret Cutie Gift Exchange Partner',
+            text: 'Happy Holidays!\n\nThe person you\'ll be gifting to is ${randomCutie()}',
         }
 
         const result = transport.sendMail(mailOptions)
@@ -42,13 +38,8 @@ async function sendMail(){
     }
 }
 
-sendMail().then((result)=> console.log('EMAIL SENT...', result))
-    
-
-
 const EMAILS = []
 
-/*
 function addNewCutie(){
     cutie_name = document.getElementById('name').value;
     cutie_email = document.getElementById('email').value;
@@ -71,6 +62,7 @@ function addNewCutie(){
 function sendToCuties(){
     sleigh_animation();
     console.log('sent')
+    generateCutiePairs()
 };
 
 function sleigh_animation(){
@@ -82,4 +74,16 @@ document.getElementById('sleigh').addEventListener('animationend', () => {
     sleigh_elem = document.getElementById('sleigh');
     sleigh_elem.style.animation = null;
 });
-*/
+
+function generateCutiePairs(){
+    const emails_copy = EMAILS.copyWithin
+    const cutie_pairs = {}
+    const cutie_first = ''
+
+    for (const i = 0; i < EMAILS.length; i++){
+        if (cutie_first){
+            Math.random() * EMAILS.length
+        }
+    }
+    Math.random() * EMAILS_UNPAIRED
+}
