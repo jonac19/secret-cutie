ce38a176f32b3d990bf73c430747f3027562b60d
jonac19 <jonac19@uci.edu>
2022-09-12 14:39:14 -0700

    Made changes to store emails in a dictionary

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const EMAILS = []
+var EMAILS = {}
 
 /**
  * Adds new friend to email list
  */
 function addNewFriend() {
-    friend_name = document.getElementById('name').value
-    friend_email = document.getElementById('email').value
+    friend_name = parseName(document.getElementById('name').value)
+    friend_email = parseEmail(document.getElementById('email').value)
     friend_list = document.getElementById('friends')
-    num_emails = EMAILS.length
+    num_emails = Object.keys(EMAILS).length
 
     // Check for valid name and email
     if (!validInput(friend_name, friend_email)) {
         return
     }
 
-    EMAILS.push(friend_email)
+    EMAILS[friend_email] = friend_name
 
     // Extends friend list in view
     if (num_emails > friend_list.children.length){
@@ -23,8 +23,26 @@ function addNewFriend() {
         friend_list.appendChild(new_friend_item)
     }
 
-    friend_item = friend_list.children[num_emails - 1]
+    friend_item = friend_list.children[num_emails]
     friend_item.textContent = friend_name
+}
+
+/**
+ * Parses the name to be capitalized
+ * @param {String} name The name of the friend
+ * @returns {String}    The capitalized name of the friend
+ */
+function parseName(name) {
+    return name[0].toUpperCase() + name.slice(1).toLowerCase()
+}
+
+/**
+ * Parse the email to be in all lowercase
+ * @param {String} email The email of the friend
+ * @returns {String}     The lowercase email of the friend
+ */
+function parseEmail(email) {
+    return email.toLowerCase()
 }
 
 /**
@@ -46,7 +64,10 @@ function validInput(name, email) {
         document.getElementById('email_error').textContent = "Please enter an email"
         valid = false
     } else if (!/^\S+@\S+\.\S+$/.test(email)) {
-        document.getElementById('email_error').textContent = "Please enter a valid email"
+        document.getElementById('email_error').textContent = "Email is invalid"
+        valid = false
+    } else if (email in EMAILS) {
+        document.getElementById('email_error').textContent = "Email already exists"
         valid = false
     } else {
         document.getElementById('email_error').textContent = ""
